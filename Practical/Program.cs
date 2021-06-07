57cb87db1e66c1be57524cafed76afdf76c55807
KADER <KADER@DESKTOP-LO6G5KU>
2021-06-07 19:49:58 +0300

    Add project files.

---
+++ Practical/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practical
+{
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("What is your favorite film?");
+                string film = Console.ReadLine();
+                Console.WriteLine("What is your favorite actor?");
+                string actor = Console.ReadLine();
+                Console.WriteLine("Film =>" + film + "  Actor =>" + actor);
+            }
+        }
+
+}
